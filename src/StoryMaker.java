abf102f10fafc70f8fba541e739f6a251a7a62da
david <davidics613@gmail.com>
2015-02-13 13:25:34 -1000

    Initial commit.

---
+++ src/StoryMaker.java
@@ -0,0 +1,14 @@
+
+public class StoryMaker {
+
+	/**
+	 * Creates story procedurally.
+	 * @param args The arguments. Not implemented.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Starting StoryMaker.");
+
+	}
+
+}
