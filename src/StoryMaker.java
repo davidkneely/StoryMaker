51c1c9b5d601250e2a75ca1364b40c13337c0019
david <davidics613@gmail.com>
2015-02-13 13:31:24 -1000

    Added Java Docs. And added agenda.

--- src/StoryMaker.java
+++ src/StoryMaker.java
@@ -1,4 +1,8 @@
-
+/**
+ * Creates stories procedurally.
+ * @author David Neely
+ *
+ */
 public class StoryMaker {
 
 	/**
@@ -6,9 +10,19 @@ public class StoryMaker {
 	 * @param args The arguments. Not implemented.
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("Starting StoryMaker.");
-
 	}
-
 }
+
+/*
+ * TODOS:
+ * Enumerate some names.
+ * Enumerate some locations.
+ * Enumerate some plot lines.
+ * Enumerate some enemies.
+ * Enumerate some conflicts.
+ * Enumerate some props.
+ * Enumerate some themes.
+ * Enumerate some tactics.
+ * Enumerate some morals.
+ */
