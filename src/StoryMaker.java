f6c3741a8ed988ffa2457a4dd3f51eb60a52717c
david <davidics613@gmail.com>
2015-02-13 14:41:35 -1000

    StoryMaker now creates a random one-line basic story.

--- src/StoryMaker.java
+++ src/StoryMaker.java
@@ -1,7 +1,7 @@
 import java.security.SecureRandom;
 
 /**
- * Creates stories procedurally.
+ * Creates story procedurally.
  * @author David Neely
  * Cited Sources: PlotLines come from Kurt Vonnegut's "Shapes of Stories."
  * http://mayaeilam.com/2012/01/01/the-shapes-of-stories-a-kurt-vonnegut-infographic/
@@ -14,17 +14,33 @@ public class StoryMaker {
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("Random from Characters: " + randomEnum(Characters.class));
-		System.out.println("Random from CharacterNames: " + randomEnum(CharacterNames.class));
-		System.out.println("Random from Locations: " + randomEnum(Locations.class));
-		System.out.println("Random from PlotLines: " + randomEnum(PlotLines.class));
-		System.out.println("Random from Antagonists: " + randomEnum(Antagonists.class));
-		System.out.println("Random from Conflicts: " + randomEnum(Conflicts.class));
-		System.out.println("Random from Props: " + randomEnum(Props.class));
+//		System.out.println("Random from Characters: " + randomEnum(Characters.class));
+//		System.out.println("Random from CharacterNames: " + randomEnum(CharacterNames.class));
+//		System.out.println("Random from Locations: " + randomEnum(Locations.class));
+//		System.out.println("Random from PlotLines: " + randomEnum(PlotLines.class));
+//		System.out.println("Random from Antagonists: " + randomEnum(Antagonists.class));
+//		System.out.println("Random from Conflicts: " + randomEnum(Conflicts.class));
+//		System.out.println("Random from Props: " + randomEnum(Props.class));
+		
+		String character = randomEnum(CharacterNames.class).name();
+		String location = randomEnum(Locations.class).name();
+		String plotLine = randomEnum(PlotLines.class).name();
+		String antagnoist = randomEnum(Antagonists.class).name();
+		String conflict = randomEnum(Conflicts.class).name();
+		String prop = randomEnum(Props.class).name();
 
-		// prints out the random story
+		// Prints out the random story
+		System.out.println(character + " was in " + location + ". " + 
+				antagnoist + " had a " + prop + ". " + character + " was stuck in a classic game of " + 
+				plotLine + ". The only way to win was to get " + conflict + ".");
+
 	}
 	
+	/**
+	 * Picks random value from Enumeration.
+	 * @param clazz The Enumeration.
+	 * @return The random value of the Enumeration.
+	 */
 	public static <T extends Enum<?>> T randomEnum(Class<T> clazz){
         int x = random.nextInt(clazz.getEnumConstants().length);
         return clazz.getEnumConstants()[x];
@@ -81,7 +97,7 @@ public class StoryMaker {
 		NewTestament,
 		Cinderalla
 	}
-	public enum Antagonists {
+	private enum Antagonists {
 		SelfEsteem,
 		Government,
 		Work,
@@ -94,7 +110,7 @@ public class StoryMaker {
 		Lover,
 		Boss
 	}
-	public enum Conflicts {
+	private enum Conflicts {
 		LostJob,
 		Marooned,
 		Ostracized,
@@ -105,7 +121,7 @@ public class StoryMaker {
 		MentalHealth,
 		HitByABus
 	}
-	public enum Props {
+	private enum Props {
 		CoffeeCup,
 		Car,
 		Plane,
@@ -119,12 +135,10 @@ public class StoryMaker {
 		Computer,
 		Book
 	}
-//	public enum Morals {
-//		
-//	}
+
 }
 
 /*
  * TODOS:
- * 
+ * Specify genders for Characters to write out correct pronouns.
  */
